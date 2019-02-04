c06e18a9eb3492cb0313be430640a3464cf86855
Ali Alsayed <a_alsayed87@hotmail.com>
2019-02-04 09:23:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,24 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients],
+                  cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
 end
