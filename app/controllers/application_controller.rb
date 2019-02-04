afa38355c9ad9ad93274ae1a717bc5617d4cf2b9
Ali Alsayed <a_alsayed87@hotmail.com>
2019-02-04 10:08:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,6 +31,12 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
 
+  patch '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(name: params[:name], ingredients: params[:ingredients],
+                   cook_time: params[:cook_time])
+  end
+
   post '/recipes' do
     @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients],
                   cook_time: params[:cook_time])
